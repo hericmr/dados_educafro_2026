b890472b39e78a73ed89bc3ed3881a1c8365d8e4
hericmr <heric.moura@unifesp.br>
2026-03-01 19:53:48 -0300

    fix: corrigindo renderização de nuvens de palavras (PlotlyError)

--- app.py
+++ app.py
@@ -36,6 +36,39 @@ st.markdown("""
 
 # App Title and Description
 st.title("Educafro | Base Parcial de Entrevistas - 2026")
+
+def check_password():
+    """Returns True if the user had the correct password."""
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if st.session_state["password"] == st.secrets["password"]:
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # don't store password
+        else:
+            st.session_state["password_correct"] = False
+
+    if "password_correct" not in st.session_state:
+        # First run, show input for password.
+        st.text_input(
+            "Por favor, insira a senha para acessar os dados:", 
+            type="password", on_change=password_entered, key="password"
+        )
+        return False
+    elif not st.session_state["password_correct"]:
+        # Password not correct, show input + error.
+        st.text_input(
+            "Por favor, insira a senha para acessar os dados:", 
+            type="password", on_change=password_entered, key="password"
+        )
+        st.error("😕 Senha incorreta")
+        return False
+    else:
+        # Password correct.
+        return True
+
+if not check_password():
+    st.stop()
+
 st.markdown("---")
 
 # Sidebar
